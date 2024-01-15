f3c16e9c97240aed36bcb4f6f0d11bfe0ed3696f
Ayush <ayushtomar8624@gmail.com>
2024-01-15 19:36:33 +0530

    practise commit one

---
+++ practise-project/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import AddUser from "./components/Users/AddUsers";
+function App() {
+  return (
+    <div>
+      <AddUser />
+    </div>
+  );
+}
+
+export default App;
